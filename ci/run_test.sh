f8bafba6a4a4ad7409375139dab4ade1550b38ba
Marcin Lulek <info@webreactor.eu>
2019-01-17 22:08:41 +0100

    testing concourse

--- ci/run_test.sh
+++ ci/run_test.sh
@@ -8,7 +8,10 @@ ls -l
 echo Build failed for commit > TEST_RUN_STATUS
 echo XXXXX >> TEST_RUN_STATUS;
 # cat .git/commit_message >> TEST_RUN_STATUS;
+
 ls -l
+cat TEST_RUN_STATUS;
+
 cd resource-channelstream-repo;
 
 pip install tox
