a016d3303587b5abf23644d68e74ed670ced0f00
Marcin Lulek <info@webreactor.eu>
2019-01-17 22:18:39 +0100

    testing concourse

--- ci/run_test.sh
+++ ci/run_test.sh
@@ -5,12 +5,12 @@ set -e
 
 ls -l
 
-echo Build failed for commit > TEST_RUN_STATUS
-echo XXXXX >> TEST_RUN_STATUS;
-# cat .git/commit_message >> TEST_RUN_STATUS;
+echo Build failed for commit > TEST_RUN_STATUS.txt
+echo XXXXX >> TEST_RUN_STATUS.txt;
+# cat .git/commit_message >> TEST_RUN_STATUS.txt;
 
 ls -l
-cat TEST_RUN_STATUS;
+cat TEST_RUN_STATUS.txt;
 
 cd resource-channelstream-repo;
 
@@ -18,7 +18,7 @@ pip install tox
 tox --skip-missing-interpreters
 cd ..;
 touch tests_passed;
-echo Build succeeded for commit > TEST_RUN_STATUS
+echo Build succeeded for commit > TEST_RUN_STATUS.txt
 echo XXXXX >> TEST_RUN_STATUS;
-# cat .git/commit_message >> TEST_RUN_STATUS;
+# cat .git/commit_message >> TEST_RUN_STATUS.txt;
 # change rev
