5cf2b98e2268e1f558c9eafea4dcc9dbc45cc054
Marcin Lulek <info@webreactor.eu>
2019-01-18 14:13:00 +0100

    testing concourse

--- ci/run_test.sh
+++ ci/run_test.sh
@@ -5,15 +5,18 @@ set -e
 
 ls -l
 
-echo Build failed for commit > TEST_RUN_STATUS.txt
-if [ -f .git/commit_message ]; then
-   cat .git/commit_message >> TEST_RUN_STATUS.txt;
+NOTIFY_FILE="notify_message/TEST_RUN_STATUS.txt"
+COMMIT_FILE=".git/commit_message"
+
+echo Build failed for commit > $NOTIFY_FILE
+if [ -f $COMMIT_FILE ]; then
+   cat $COMMIT_FILE >> $NOTIFY_FILE;
 else
-   echo Unknown >> TEST_RUN_STATUS.txt;
+   echo Unknown >> $NOTIFY_FILE;
 fi
 
 ls -l
-cat TEST_RUN_STATUS.txt;
+cat $NOTIFY_FILE;
 
 cd resource-channelstream-repo;
 
@@ -21,9 +24,9 @@ pip install tox
 tox --skip-missing-interpreters
 cd ..;
 touch tests_passed;
-echo Build succeeded for commit > TEST_RUN_STATUS.txt
-if [ -f .git/commit_message ]; then
-   cat .git/commit_message >> TEST_RUN_STATUS.txt;
+echo Build succeeded for commit > $NOTIFY_FILE
+if [ -f $COMMIT_FILE ]; then
+   cat $COMMIT_FILE >> $NOTIFY_FILE;
 else
-   echo Unknown >> TEST_RUN_STATUS.txt;
+   echo Unknown >> $NOTIFY_FILE;
 fi
