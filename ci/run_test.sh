cb691727f65dcf6e9c37a62bb9c26f867db5ba9d
Marcin Lulek <info@webreactor.eu>
2019-01-18 14:21:51 +0100

    testing concourse

--- ci/run_test.sh
+++ ci/run_test.sh
@@ -7,23 +7,23 @@ ls -l
 
 NOTIFY_FILE="notify_message/TEST_RUN_STATUS.txt"
 COMMIT_FILE=".git/commit_message"
+APP_NAME="Channelstream"
 
-echo Build failed for commit > $NOTIFY_FILE
+echo $APP_NAME tests > $NOTIFY_FILE
+
 if [ -f $COMMIT_FILE ]; then
    cat $COMMIT_FILE >> $NOTIFY_FILE;
 else
    echo Unknown >> $NOTIFY_FILE;
 fi
 
-ls -l
-cat $NOTIFY_FILE;
 
 cd resource-channelstream-repo;
 
 pip install tox
 tox --skip-missing-interpreters
 cd ..;
-touch tests_passed;
+
 echo Build succeeded for commit > $NOTIFY_FILE
 if [ -f $COMMIT_FILE ]; then
    cat $COMMIT_FILE >> $NOTIFY_FILE;
